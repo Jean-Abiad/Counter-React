9277b341a4bb76ea4dd3958a04104fe07ed6e454
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-03-03 16:50:25 -0400

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Cards from "./Cards";
+//include images into your bundle
+
+
+//create your first component
+const Home = (props) => {
+	return <>
+		<div className=" d-flex justify-content-center align-items-center" >
+			<Cards icon />
+			<Cards num={Math.floor((props.seconds % 1000000) / 100000)} />
+			<Cards num={Math.floor((props.seconds % 100000) / 10000)} />
+			<Cards num={Math.floor((props.seconds % 10000) / 1000)} />
+			<Cards num={Math.floor((props.seconds % 1000) / 100)} />
+			<Cards num={Math.floor((props.seconds % 100) / 10)} />
+			<Cards num={props.seconds % 10} />
+		</div>
+
+	</>
+
+
+};
+
+export default Home;
---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+// include your styles into the webpack bundle
+import "../styles/index.css";
+
+//import your own components
+import Home from "./component/home.jsx";
+
+//render your react application
+
+let contador = 0
+
+
+function renderizado ( ) {
+     contador = contador + 1
+     ReactDOM.render(<Home seconds={contador} />, document.querySelector("#app"));
+} 
+
+setInterval(renderizado, 1000)
+
+
+
+
